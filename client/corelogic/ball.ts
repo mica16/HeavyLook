3d0283f648a85cc7a72a3436813cab35e12aa06b
Michael Azerhad <m.azerhad@wealcome.com>
2016-12-05 04:17:23 +0100

    remove useless weigh method

--- client/corelogic/ball.ts
+++ client/corelogic/ball.ts
@@ -3,10 +3,6 @@ export default class Ball {
     constructor(private _position: number, private _heaviest: boolean = false) {
     }
 
-    weigh() {
-        return new Ball(this._position, true);
-    }
-
     get heaviest(): boolean {
         return this._heaviest;
     }
